46102f240bc2b44d661e506e52fa582bab4b47ec
Saikumar1245 <139545470+Saikumar1245@users.noreply.github.com>
2023-07-17 16:10:24 +0530

    Update class1.java

--- project/src/main/java/pkg1/class1.java
+++ project/src/main/java/pkg1/class1.java
@@ -4,6 +4,8 @@ public class class1
 {
 	public static void T1()
 	{
+		System.out.println("hi how are mmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+		System.out.println("hi how are mmmmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
 		System.out.println("hi how are you");
 		System.out.println("hi how are Fsd");
 		System.out.println("hi how are ger");
